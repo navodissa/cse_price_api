357c39ed5e49620491f00b5eb226fd408cd57239
Navoda <navomails@gmail.com>
2021-09-15 22:26:47 +0000

    Updated the log file

--- cse_price_api/settings.py
+++ cse_price_api/settings.py
@@ -2,7 +2,7 @@
 # @Author: Your name
 # @Date:   2021-09-13 14:12:16
 # @Last Modified by:   Your name
-# @Last Modified time: 2021-09-15 22:24:29
+# @Last Modified time: 2021-09-15 22:26:28
 """
 Django settings for cse_price_api project.
 
@@ -151,7 +151,7 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': 'get_price/logs/debug.log',
+            'filename': 'get_price/debug.log',
             'formatter': 'verbose',
         },
     },
