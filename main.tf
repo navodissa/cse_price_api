e0e41a680de573d0ff3263635c1d0c7d9916e0d9
Navoda <navomails@gmail.com>
2021-09-19 22:39:43 +0000

    Updated with Migration

--- main.tf
+++ main.tf
@@ -43,8 +43,8 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 8000
+    to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
