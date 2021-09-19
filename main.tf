5ea53f5206d498b839c1f8d1def429a26aecc8c5
Navoda <navomails@gmail.com>
2021-09-19 19:52:46 +0000

    updated

--- main.tf
+++ main.tf
@@ -31,6 +31,7 @@ resource "aws_instance" "web" {
   ami                    = "ami-830c94e3"
   instance_type          = "t2.micro"
   key_name = "tf-testing"
+  count = 2
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
