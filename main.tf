eb81db2f31ba609242d07918d4093a2d6176ef5e
Navoda <navomails@gmail.com>
2021-09-19 15:40:18 +0000

    aws-4

--- main.tf
+++ main.tf
@@ -29,13 +29,6 @@ provider "aws" {
 
 resource "random_pet" "sg" {}
 
-module "key_pair" {
-  source = "terraform-aws-modules/key-pair/aws"
-
-  key_name = "navoda_me_keypair"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAgMJhIV64QvB/xqW3LiFx733LP5aND5JJt3a7To5Iu9EmXp9DwXeTMAmRzta8TjG+V+JviJlBNCp66dBrAAPs2BowssKbTTLMrqtjN4Y2fy28+NGHaogzd3A8Jia9O4gtV9NZfws7l24scZDpxS/hK5wgPs5oHBj/q2stfaoMtdzwh8y8FbdRrQHW6Us4zBRz0tPRk9ybvkyfnkZcVir6zJfypnf/+6W6xm4wFfXmXxKVjgV3TyGIC+WE5tJgxVRqUZoUcWX3pF66zJ8ATdrbQeZLMdEDguCjCXld73giTp7A/ry79Q+gbXBllGjik5pIbv2/tWatF9qw4xU/CX8UKQ== imported-openssh-key"
-}
-
 resource "aws_instance" "web" {
   ami                    = "ami-830c94e3"
   instance_type          = "t2.micro"
